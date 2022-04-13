2d8748fd30088c90efbc0851ba9e737f65bc33d1
ferdi inan <ferdiinan@hotmail.com>
2022-04-13 23:39:40 +0300

    şehir bilgisi sorgulama eklendi.

---
+++ MM_Mobil_Api_Entegrasyon_Modulu/Cities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM_Mobil_Api_Entegrasyon_Modulu
+{
+    public class Cities
+    {
+        public string id { get; set; }
+        public int code { get; set; }
+        public string name { get; set; }
+
+        public List<Cities> CitiesList()
+        {
+
+            string url = "/parameter/cities";
+
+            var client = new Api.Client<Cities>();
+
+            return client.GetMethodList(url);
+        }
+    }
+}
