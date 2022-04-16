583080bafa7e3f7714775bc59c0bcfee74676bb1
ferdi inan <ferdiinan@hotmail.com>
2022-04-16 17:34:19 +0300

    Update Refund Methodu Eklendi.

---
+++ MM_Mobil_Api_Entegrasyon_Modulu/Orders/RefundUpdate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM_Mobil_Api_Entegrasyon_Modulu.Orders
+{
+    public class RefundUpdate
+    {
+        private string RefundId;
+        private int Status;
+
+        public RefundUpdate(string RefundId,int Status)
+        {
+            this.RefundId = RefundId;
+            this.Status = Status;
+        }
+
+        public RefundUpdateResponse UpdateRefund()
+        {
+
+            string url = "/order/updateRefund";
+
+            var client = new Api.Client<RefundUpdateResponse>();
+
+            var result = client.PostMethod(this, url);
+
+            return result;
+        }
+
+        public string refundId { get { return this.RefundId; } set { this.RefundId = value; } }
+        public int status { get { return this.Status; } set { this.Status = value; } }
+    }
+}
---
+++ MM_Mobil_Api_Entegrasyon_Modulu/Orders/RefundUpdateResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM_Mobil_Api_Entegrasyon_Modulu.Orders
+{
+    public class RefundUpdateResponse
+    {
+        public int orderNumber { get; set; }
+        public int requestNo { get; set; }
+        public string refundId { get; set; }
+        public string orderItemId { get; set; }
+        public string refundType { get; set; }
+        public string refundStatus { get; set; }
+    }
+}
