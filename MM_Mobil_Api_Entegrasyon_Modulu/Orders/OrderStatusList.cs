cf330fa8ccc274a93f58f34ed781412cfb40f960
ferdi inan <ferdiinan@hotmail.com>
2022-04-16 16:04:12 +0300

    Update Order Status List Methodu Eklendi.

---
+++ MM_Mobil_Api_Entegrasyon_Modulu/Orders/OrderStatusList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM_Mobil_Api_Entegrasyon_Modulu.Orders
+{
+    public class OrderStatusList
+    {
+        private int OrderNumber;
+        private int Status;
+
+        public OrderStatusList(int OrderNumber,int Status)
+        {
+
+            this.OrderNumber = OrderNumber;
+            this.Status = Status;
+        }
+
+        public void UpdateOrderStatusList()
+        {
+
+            string url = "/order/updateOrderStatusList";
+
+            var client = new Api.Client<HelperResponse>();
+
+            var result = client.PutMethod(this, url);
+
+            if (!result.success) throw new ArgumentException("Sipariş durum bilgisi güncellenemedi. " + result.message);
+        }
+    }
+}
