856f003f701ebe442e8fa71242f7c53d16955146
ferdi inan <ferdiinan@hotmail.com>
2022-04-16 17:48:26 +0300

    Get Payment List Methodu Eklendi.

---
+++ MM_Mobil_Api_Entegrasyon_Modulu/Orders/PaymentList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM_Mobil_Api_Entegrasyon_Modulu.Orders
+{
+    public class PaymentList
+    {
+        public string transactionId { get; set; }
+        public int orderNumber { get; set; }
+        public decimal price { get; set; }
+        public string currencyCode { get; set; }
+        public string status { get; set; }
+        public DateTime paymentperioddate { get; set; }
+        public decimal commission { get; set; }
+        public decimal commissionRate { get; set; }
+        public int paymentPeriod { get; set; }
+        public DateTime orderDate { get; set; }
+
+        public List<PaymentList> GetPaymentList(long startDate,long endDate)
+        {
+
+            string url = $"/getPaymentList?startDate={startDate}&endDate={endDate}";
+
+            var client = new Api.Client<PaymentList>();
+
+            var result = client.GetMethodList(url);
+
+            return result;
+        }
+    }
+}
